f0846b1bf923587a54e74a1941703d96886a82aa
Usuario <Usuario@DESKTOP-P22LJR3>
2023-11-13 20:08:08 -0500

    Agregar archivos de proyecto.

---
+++ Facturera.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factura
+{
+    public partial class Facturera : Form
+    {
+        public Facturera()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float precio;
+            string nom;
+            int cod;
+
+            cod = cmbProducto.SelectedIndex;
+            nom = cmbProducto.SelectedItem.ToString();
+            precio = cmbProducto.SelectedIndex;
+
+            switch (cod)
+            {
+                case 0: lblCodigo.Text = "0011";break;
+                case 1: lblCodigo.Text = "0022";break;
+                case 2: lblCodigo.Text = "0033"; break;
+                default: lblCodigo.Text = "0044";break;
+            }
+
+            switch (nom)
+            {
+                case "Nutela": lblNombre.Text = "Nutela";break;
+                case "Arequipe": lblNombre.Text = "Arequipe";break;
+                case "Maracuya": lblNombre.Text = "Maracuya"; break;
+                default: lblNombre.Text = "Chocolate";break;
+            }
+
+            switch (precio)
+            {
+                case 0: lblPrecio.Text = "4000";break;
+                case 1: lblPrecio.Text = "4000";break;
+                case 2: lblPrecio.Text = "5000"; break;
+                default: lblPrecio.Text = "4000";break;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow file = new DataGridViewRow();
+            file.CreateCells(dgvLista);
+
+            file.Cells[0].Value = lblCodigo.Text;
+            file.Cells[1].Value = lblNombre.Text;
+            file.Cells[2].Value = lblPrecio.Text;
+            file.Cells[3].Value = txtCantidad.Text;
+            file.Cells[4].Value = (float.Parse(lblPrecio.Text) * float.Parse(txtCantidad.Text)).ToString();
+
+            dgvLista.Rows.Add(file);
+
+            lblCodigo.Text = lblNombre.Text = lblPrecio.Text = txtCantidad.Text = "";
+
+            obtenerTotal();
+
+
+        }
+
+        public void obtenerTotal()
+        {
+            float costot = 0;
+            int contador = 0;
+
+            contador = dgvLista.RowCount;
+
+            for (int i = 0; i < contador; i++)
+           {
+                costot += float.Parse(dgvLista.Rows[i].Cells[4].Value.ToString());
+            }
+
+            lblTotatlPagar.Text = costot.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar producto?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (rppta == DialogResult.Yes)
+                {
+                    dgvLista.Rows.Remove(dgvLista.CurrentRow);
+                }
+            }
+            catch { }
+            obtenerTotal();
+        }
+
+        private void txtEfectivo_TextChanged(object sender, EventArgs e)
+        {
+            try {
+                lblDevolucion.Text = (float.Parse(txtEfectivo.Text) - float.Parse(lblTotatlPagar.Text)).ToString();
+
+                
+            }
+            catch { }
+
+            if (txtEfectivo.Text == "")
+            {
+                lblDevolucion.Text = "";
+            }
+
+        }
+
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+            CLSFacturera.CreaTicket Ticket1 = new CLSFacturera.CreaTicket();
+
+            Ticket1.TextoCentro("DONA NOSTRA "); //imprime una linea de descripcion
+            Ticket1.TextoCentro("**********************************");
+
+            Ticket1.TextoIzquierda("");
+            Ticket1.TextoCentro("Factura de Venta"); //imprime una linea de descripcion
+            Ticket1.TextoIzquierda("No Fac: 0120102");
+            Ticket1.TextoIzquierda("Fecha:" + DateTime.Now.ToShortDateString() + " Hora:" + DateTime.Now.ToShortTimeString());
+            Ticket1.TextoIzquierda("Le Atendio: xxxx");
+            Ticket1.TextoIzquierda("");
+            CLSFacturera.CreaTicket.LineasGuion();
+
+            CLSFacturera.CreaTicket.EncabezadoVenta();
+           CLSFacturera.CreaTicket.LineasGuion();
+            foreach (DataGridViewRow r in dgvLista.Rows)
+            {
+                // PROD                     //PrECIO                                    CANT                         TOTAL
+                Ticket1.AgregaArticulo(r.Cells[1].Value.ToString(), double.Parse(r.Cells[2].Value.ToString()), int.Parse(r.Cells[3].Value.ToString()), double.Parse(r.Cells[4].Value.ToString())); //imprime una linea de descripcion
+            }
+
+
+            CLSFacturera.CreaTicket.LineasGuion();
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.AgregaTotales("Total", double.Parse(lblTotatlPagar.Text)); // imprime linea con total
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.AgregaTotales("Efectivo Entregado:", double.Parse(txtEfectivo.Text));
+            Ticket1.AgregaTotales("Efectivo Devuelto:", double.Parse(lblDevolucion.Text));
+
+
+            // Ticket1.LineasTotales(); // imprime linea 
+
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoCentro("*     Gracias por preferirnos    *");
+
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoIzquierda(" ");
+            string impresora = "Microsoft XPS Document Writer";
+            Ticket1.ImprimirTiket(impresora);
+
+            MessageBox.Show("Gracias por preferirnos");
+
+            this.Close();
+        }
+
+        private void lblDevolucion_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
